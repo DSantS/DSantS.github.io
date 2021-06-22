b29fe4b796dbb8c33e37b4c9b41da634b11f0ad4
DSantS <luissanta1998@gmail.com>
2021-06-21 21:04:41 -0500

    Commit 24 Plusss del plusss Ulra

--- js/principal.js
+++ js/principal.js
@@ -1,16 +1,3 @@
-/*$(function () {
-  var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  var time = today.getHours() + ":" + today.getMinutes();
-  var dateTime = date+' '+time;
-  $("#form_datetime").datetimepicker({
-      format: 'yyyy-mm-dd hh:ii',
-      autoclose: true,
-      todayBtn: true,
-      startDate: dateTime
-  });
-});*/
-
 
 $(document).ready(function(){
     
